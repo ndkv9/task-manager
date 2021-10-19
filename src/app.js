6cf48c1b6e4807a8694b5f33ce99a03fbce9a496
Vu Nguyen <vu.nguyen101@gmail.com>
2021-10-19 21:08:42 +0300

    setup basic express app

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const app = express()
+
+app.use(cors())
+app.use(helmet())
+app.use(express.json())
+
+app.get('/ping', (_req, res) => {
+  res.send('pong!')
+})
+
+exports = app
