01bcd9ec701d9a2c52098883666faeec0377f909
Vu Nguyen <vu.nguyen101@gmail.com>
2021-10-22 12:15:06 +0300

    implement updateTask fn

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -19,8 +19,25 @@ const createTask = async (req, res) => {
   res.status(201).json({ task })
 }
 
-const updateTask = (req, res) => {
-  res.send('update a task')
+const updateTask = async (req, res) => {
+  const id = req.params.id
+  const body = req.body
+  const taskToUpdate = await Task.findById(id)
+
+  if (!taskToUpdate) {
+    return res.status(404).json({ msg: `cannot found task with id ${id}` })
+  }
+
+  const taskObj = {
+    ...body,
+  }
+
+  const returnedTask = await Task.findByIdAndUpdate(id, taskObj, {
+    new: true,
+    runValidators: true,
+  })
+
+  return res.status(200).json({ task: returnedTask })
 }
 
 const deleteTask = async (req, res) => {
